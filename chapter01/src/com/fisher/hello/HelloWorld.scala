d08748c85db6d84c339151b586e3e944ee5bbb0a
zhuweifeng <675748324@qq.com>
2016-07-29 14:12:33 +0800

    第一个git项目测试

---
+++ chapter01/src/com/fisher/hello/HelloWorld.scala
@@ -0,0 +1,10 @@
+package com.fisher.hello
+
+/**
+  * Created by admin on 2016/7/29.
+  */
+object HelloWorld {
+    def main(args: Array[String]) {
+	println("Hello, My first git program!")
+    }
+}
